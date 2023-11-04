7b9548b22a5ef17a2b172ba0da2cfc8d09e92904
Luleee <luleee@elfak.rs>
2023-11-04 22:29:23 +0100

    lab2PAA

---
+++ paa/lab2/generate_files.py
@@ -0,0 +1,17 @@
+import random
+
+letters_ascii = [chr(i) for i in range(128)]
+letters_hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+alphabets = {'ascii': letters_ascii, 'hex': letters_hex}
+sizes = [100, 1000, 10_000, 100_000]
+
+
+for size in sizes:
+    for name, alphabet in alphabets.items():
+
+        with open(f'{name}_{size}.txt', 'w+', encoding='ascii') as f:
+            for _ in range(size):
+                word_length = random.randint(1, 100)
+                random_word = ''.join(random.choice(alphabet) for _ in range(word_length))
+                f.write(f'{random_word}{" "}')
--- paa/lab2/lab2.py
+++ paa/lab2/lab2.py
@@ -31,8 +31,101 @@ def rabin_karp(target_string: str, pattern: str, alphabet: dict):
             j += 1
         if j == m:
             print(ind)
+
+
+def failure_func(pattern: str) -> list[int]:
+    pi = [0] * len(pattern)
+    pi[0] = -1
+    for i in range(1, len(pattern)):
+        j = pi[i - 1]
+        while j > 0 and pattern[i] != pattern[j]:
+            j = pi[j - 1]
+        if pattern[i] == pattern[j]:
+            j += 1
+        pi[i] = j
+    return pi
+
+def knuth_morris_pratt(target_string: str, pattern: str):
+    pi = failure_func(pattern)
+    j = -1
+
+    i = 0
+    while i < len(target_string):
+        if target_string[i] == pattern[j + 1]:
+            i += 1
+            j += 1        
+            if j + 1 == len(pattern):
+                print(i - len(pattern))
+                j = pi[j]
+        else:
+            if j == -1:
+                i += 1
+            else:
+                j = pi[j]
+
+
+letter_to_number = {
+    'b': 1, 'f': 1, 'p': 1, 'v': 1,
+    'c': 2, 'g': 2, 'j': 2, 'k': 2, 'q': 2, 's': 2, 'x': 2, 'z': 2,
+    'd': 3, 't': 3,
+    'l': 4,
+    'm': 5, 'n': 5,
+    'r': 6
+}
+
+def soundEx(string: str):
+    neutrals = {'a', 'e', 'i', 'o', 'u', 'y', 'h', 'w'}
+    seen = {}
+    strng = ''
+    for i, letter in enumerate(string):
+        if letter in neutrals:
+            seen[letter] = i
+        elif letter_to_number[letter.lower()] in seen:
+            idx = seen[letter_to_number[letter.lower()]]
+            if i - idx == 1 or (i - idx == 2 and string[i - 1] in {'h', 'w'}):
+                seen[letter_to_number[letter.lower()]] = i
+            else:
+                strng += letter
+        else:
+            seen[letter_to_number[letter.lower()]] = i
+            strng += letter
+
+    new_string = string[0]
+    for i in range(1, len(strng)):
+        new_string += str(letter_to_number[strng[i].lower()])
+
+    if len(new_string) < 4:
+        new_string += '0' * (4 - len(new_string))
+    else:
+        new_string = new_string[0:4]
+    print(new_string)
+
+
+def levenshtein(str1: str, str2: str) -> int:
+    matrix = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
     
+    for i in range(1, len(str1) + 1):
+        matrix[i][0] = matrix[i - 1][0] + 1
+    for j in range(1, len(str2) + 1):
+        matrix[0][j] = matrix[0][j - 1] + 1
 
-ascii_dict = {chr(i):i for i in range(128)}
+    for i in range(len(str1)):
+        for j in range(len(str2)):
+            if str1[i] == str2[j]:
+                inc = 0
+            else:
+                inc = 1
+            matrix[i + 1][j + 1] = min(matrix[i][j] + inc, matrix[i + 1][j] + 1, matrix[i][j + 1] + 1)
+    
+    return matrix[len(str1)][len(str2)]
 
-rabin_karp('Ovo je targetarget string target', 'target', ascii_dict)
+levenshtein('sitting', 'kitten')
+
+# ascii_dict = {chr(i):i for i in range(128)}
+
+# target = 'Ovo je targetarget string target'
+# pattern = 'target'
+
+# rabin_karp(target, pattern, ascii_dict)
+# knuth_morris_pratt(target, pattern)
+
