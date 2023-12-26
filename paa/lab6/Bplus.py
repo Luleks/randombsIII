4625d1499d05d42837b928b4c6131a2649fb330a
Luleee <luleee@elfak.rs>
2023-12-26 13:49:07 +0100

    lab6...

--- paa/lab6/Bplus.py
+++ paa/lab6/Bplus.py
@@ -10,6 +10,7 @@ class BplusNode:
         self.pointers: list["BplusNode"] = []
         self.is_leaf: bool = is_leaf
         self.next_leaf: BplusNode | None = None
+        self.prev_leaf: BplusNode | None = None
         self.parent: BplusNode | None = None
 
     @staticmethod
@@ -25,7 +26,10 @@ class BplusNode:
             else:
                 new_node = BplusNode(True)
                 new_node.parent = node.parent
+                if node.next_leaf:
+                    node.next_leaf.prev_leaf = new_node
                 node.next_leaf = new_node
+                new_node.prev_leaf = node
                 middle_key = node.keys[2 * BplusNode.d // 2]
                 new_node.keys = node.keys[2 * BplusNode.d // 2: ]
                 node.keys = node.keys[0: 2 * BplusNode.d // 2]
@@ -62,6 +66,55 @@ class BplusNode:
                     node = node.parent
                     BplusNode.append_key(node, middle_key, node, new_node)
                     return None
+    
+    @staticmethod
+    def remove_key(node: "BplusNode", key: int, deleted_node: "BplusNode" = None):
+        if node.is_leaf:
+            node.keys.remove(key)
+            if len(node.keys) < BplusNode.d:
+                sibling_node = node.next_leaf if node.next_leaf and node.next_leaf.parent is node.parent else node.prev_leaf
+                node1 = node if node.next_leaf is sibling_node else sibling_node
+                node2 = node if node.prev_leaf is sibling_node else sibling_node
+                if len(sibling_node) == BplusNode.d:
+                    node2.keys = node1.keys + node2.keys
+                    node2.prev_leaf = node1.prev_leaf
+                    return BplusNode.remove_key(node2.parent, 0, node1)
+
+                else:
+                    keys = node.keys + sibling_node.keys
+                    node1.keys = keys[:len(keys) // 2] 
+                    node2.keys = keys[len(keys) // 2:]
+            return None
+        else:
+            idx = node.pointers.index(deleted_node)
+            keys.pop(idx)
+            node.pointers.remove(deleted_node)
+            if len(node.keys) < BplusNode.d and node.parent:
+                par_idx = node.parent.pointers.index(node)
+                sibling_node1 = node.parent.pointers[par_idx - 1] if par_idx > 0 else None
+                sibling_node2 = node.parent.pointers[par_idx + 1] if par_idx < len(node.parent.pointers) - 1 else None
+                if (sibling_node1 and len(sibling_node1.keys) > BplusNode.d):
+                    keys = sibling_node1.keys + [node.parent.keys[idx - 1]] + node.keys
+                    node.keys = keys[-BplusNode.d:]
+                    node.parent.keys[idx - 1] = keys[-BplusNode.d - 1]
+                    sibling_node1.keys[:-BplusNode.d - 1]
+                    node.pointers.insert(0, sibling_node1.pointers[-1])
+                    sibling_node1.pointers.pop()
+                elif (sibling_node2 and len(sibling_node2.keys) > BplusNode.d):
+                    keys = node.keys + [node.parent.keys[idx]] + sibling_node2.keys
+                    node.keys = keys[:BplusNode.d]
+                    node.parent.keys[idx] = keys[BplusNode.d]
+                    sibling_node2.keys = keys[BplusNode.d + 1:]
+                    node.pointers.append(sibling_node2.pointers[0])
+                    sibling_node2.pointers.pop(0)
+                else:
+                    if sibling_node1:
+                        pass
+                    else:
+                        pass
+
+            return None
+
     def __repr__(self):
         return f'{self.keys}'
                 
@@ -97,6 +150,8 @@ class BplusTree:
             self.root.pointers[0].parent = self.root
             self.root.pointers.append(BplusNode(True))
             self.root.pointers[1].parent = self.root
+            self.root.pointers[0].next_leaf = self.root.pointers[1]
+            self.root.pointers[1].prev_leaf = self.root.pointers[0]
             return
 
         leaf_node, exists = self.find(key)
@@ -111,6 +166,15 @@ class BplusTree:
         if new_root is not None:
             self.root = new_root
         return True
+    
+    def remove(self, key) -> bool:
+        leaf_node, exists = self.find(key)
+        if not exists:
+            return False
+        
+        BplusNode.remove_key(leaf_node, key)
+
+
     
     def print_tree(self):
         
@@ -146,9 +210,18 @@ if __name__ == "__main__":
 
     tree.insert(88)
 
+    prev = None
     temp, _ = tree.find(5)
     while temp:
+        prev = temp
         for key in temp.keys:
-            print(key)
+            print(key, end = ' ')
+        print("|", end=' ')
         temp = temp.next_leaf
-
+    print()
+    while prev:
+        for key in prev.keys:
+            print(key, end=' ')
+        print("|", end=' ')
+        prev = prev.prev_leaf
+    print()
